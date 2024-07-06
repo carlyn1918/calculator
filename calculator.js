dba28d3f5bb44c6af7d61f57a2395312cee037c6
Carlo Lirazan <133843215+carlyn1918@users.noreply.github.com>
2024-07-07 02:01:52 +0800

    Added parseFloat

--- calculator.js
+++ calculator.js
@@ -1,16 +1,79 @@
 const readline = require('readline');
-
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+const inputNums = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
 });
 
-function displayMenu() {
-    console.log('Choose:');
-    console.log('1. +');
-    console.log('2. -');
-    console.log('3. *');
-    console.log('4. /');
+let num1, num2;
+let count = 0;
+let operatorChosen;
+let sum = (num1 + num2);
+
+const addOp = () => {
+  console.log (num1 + num2);
 };
-// test call function
-displayMenu();
+
+console.log('Enter first number: ');
+
+inputNums.on('line', (input) => {
+  if (count === 0){
+    num1 = parseFloat(input.trim());
+    console.log(`First number is: ${num1}`);
+    console.log(`Enter second number: `);
+    count++;
+  } else if (count === 1) {
+    num2 = parseFloat(input.trim());
+    console.log(`The second number is: ${num2}`);
+    console.log('Choose an operation: enter number 1 - 4');
+    console.log('1. Add');
+    console.log('2. Subtract');
+    console.log('3. Multiply');
+    console.log('4. Divide');
+    count++;
+    
+  } else if (count === 2) {
+    operatorChosen = input.trim();
+    console.log(`You choose the operator: ${operatorChosen}`);
+        console.log(`The answer is:`);
+        count++;
+        if (count === 3 && operatorChosen === '1'){
+            addOp();
+          }  else if (count === 3 && operatorChosen === '2'){
+            subOp();
+          }
+    
+    inputNums.close();
+  } 
+  
+  
+  else if (operatorChosen === 2){
+    subOp();
+  } else if (operatorChosen === 3){
+    mulOp();
+  } else if (operatorChosen === 4){
+    divOp();
+   
+  } else {
+    console.log('Invalid input: run the script again!');
+  }
+});
+
+
+
+
+
+
+const subOp = () => {
+  console.log (num1 - num2);
+};
+
+const mulOp = () => {
+  console.log (num1 * num2);
+};
+
+const divOp = () => {
+  console.log (num1 / num2);
+};
+
+
+
