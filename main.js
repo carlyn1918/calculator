232e9572ef63e412168c9195e27e2577958f735f
Carlo Lirazan <133843215+carlyn1918@users.noreply.github.com>
2024-07-09 01:35:16 +0800

    testing the input lines

---
+++ main.js
@@ -0,0 +1,16 @@
+const readline = require('readline');
+const inputNums = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+let ops1 = '';
+let A1 = 4;
+let B1 = 3;
+
+console.log('Enter what you wanted to compute here (Example 1+1*2) ');
+
+inputNums.on('line', (input) => {
+    ops1 = input.trim();
+    console.log(`You want to calculate this ${ops1}`);
+}); // this code works - shows trimed inputs
