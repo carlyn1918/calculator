7604d93e7ebd4eeba7544739212530986746962c
Carlo Lirazan <133843215+carlyn1918@users.noreply.github.com>
2024-07-09 03:53:23 +0800

    Fixed - used if instead of else if in line 33

--- main.js
+++ main.js
@@ -9,29 +9,39 @@ let A1 = 4;
 let B1 = 3;
 let count = 0;
 let results = eval(ops1);
-let choice = 0; 
+let choice = 1; 
 
-console.log('Enter what you wanted to compute here (Example 1+1*2) ');
+console.log('Enter what you wanted to compute here: (Example 1+1*2) ');
 
 inputNums.on('line', (input) => {
-    if (count === 0) {
+    if (count === 0 && choice === 1) {
     ops1 = input.trim();
     console.log(`You want to calculate this ${ops1}`);
     count++;
-    console.log('testing to skip the prompt for enter key');
     let results = eval(ops1);
     console.log(`the results is ${results}`);
     console.log(`Do you want to try again? Press 1 to try again`);
-    choice = parseFloat(input.trim());
-    } else if (count === 1) {
+    
+    } else if (count === 1 ) {
+        
         choice = parseFloat(input.trim());
+        console.log(`the choice is ${choice}`);
         console.log('Enter what you wanted to compute here (Example 1+1*2) ');
         count = 0;
+    
+
+    if ( choice === 2 ){
+        console.log('it worked its number 2 - choice');
+      };
+    }
         
         
-    } else if (count === 1 && choice > 1){
+        
+        
+     else {
         console.log('ending the scripts!');
         inputNums.close();
+        console.log(choice);
     } 
 }); // this code works - shows trimed inputs
 
